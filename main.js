1bf133fdf2d8880a05d01bfd2e57663700a2b4b1
Frinze Matabalan <codewithfrinze@gmail.com>
2023-11-06 19:37:01 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 
 const navbar = document.querySelector('.header');
 const targetDiv1 = document.querySelector('#about-phase-one');
-const targetDiv2 = document.querySelector('#projects'); 
-const targetDiv3 = document.querySelector('#contacts'); 
+const targetDiv2 = document.querySelector('#about-phase-two'); 
+const targetDiv3 = document.querySelector('#projects');
+
+const targetDiv4 = document.querySelector('#contacts'); 
+
 const navItems = document.querySelectorAll('.header-links'); 
 
+const navbarName = document.querySelector('#name')
 const navbarHome = document.querySelector('#nav-home')
 const navbarAbout = document.querySelector('#nav-about')
 const navbarProjects = document.querySelector('#nav-projects')
@@ -15,6 +19,7 @@ const navbarContacts = document.querySelector('#nav-contacts')
 const targetDiv1Position = targetDiv1.offsetTop;
 const targetDiv2Position = targetDiv2.offsetTop;
 const targetDiv3Position = targetDiv3.offsetTop;
+const targetDiv4Position = targetDiv4.offsetTop;
 
 
 function updateNavbarStyles() {
@@ -32,15 +37,30 @@ function updateNavbarStyles() {
     }
     
     if (scrollPosition >= targetDiv1Position && scrollPosition < targetDiv2Position) {
+        navbarName.style.color = 'white';
         navbar.style.background = 'black'; 
-        navbarAbout.style.color = 'rgb(114, 33, 33)'
-        navbarHome.style.color = 'white'
+        navbarAbout.style.color = 'rgb(114, 33, 33)';
+        navbarHome.style.color = 'white';
+    } else if (scrollPosition >= targetDiv4Position) {
+        navbar.style.background = 'black';
+        navbarName.style.color = 'white';
+        navbarHome.style.color = 'white';
+        navbarAbout.style.color = 'white';
+        navbarProjects.style.color= 'white';
+        navbarContacts.style.color= 'rgb(114, 33, 33)';
     } else if (scrollPosition >= targetDiv3Position) {
         navbar.style.background = 'linear-gradient(90deg, rgba(178,223,222,1) 1%, rgba(56,120,89,1) 51%, rgba(68,19,23,1) 99%)';
-        navbarContacts.style.color= 'white' 
+        navbarName.style.color = 'white';
+        navbarHome.style.color = 'white';
+        navbarAbout.style.color = 'white';
+        navbarContacts.style.color = 'white';
+        navbarProjects.style.color= 'rgb(114, 33, 33)';
+
     } else if (scrollPosition >= targetDiv2Position) {
+        navbarName.style.color = 'white';
+        navbarName.style.color = 'black';
         navbar.style.background = 'white'; 
-        navbarProjects.style.color= 'rgb(114, 33, 33)' 
+        navbarProjects.style.color= 'rgb(114, 33, 33)';
     } else {
         navbar.style.background = 'linear-gradient(90deg, rgba(178,223,222,1) 1%, rgba(56,120,89,1) 51%, rgba(68,19,23,1) 99%)';
     }
